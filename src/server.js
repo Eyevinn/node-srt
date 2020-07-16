332c7d0f44c9870073aa66dadd3e541df0a9a462
Stephan Hesse <tchakabam@gmail.com>
2020-07-16 04:02:07 +0200

    server.js: set "iface" and "port" properties on SRTServer listen call + and init them with null in constructor

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ const libSRT = new SRT();
 class SRTServer extends EventEmitter {
   constructor() {
     super();
+    this.iface = null;
+    this.port = null;
     this.socket = libSRT.createSocket();
   }
 
@@ -14,6 +16,8 @@ class SRTServer extends EventEmitter {
     const iface = address || "0.0.0.0";
     libSRT.bind(this.socket, iface, port);
     libSRT.listen(this.socket, 2);
+    this.iface = address;
+    this.port = port;
     this.emit("listening", iface, port);
     while (true) {
       const fhandle = libSRT.accept(this.socket);
