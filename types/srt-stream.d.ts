eac6e4ffc557d746593b7413887b943741816009
Stephan Hesse <tchakabam@gmail.com>
2020-07-16 03:58:48 +0200

    add typescript definition files for this JS API

---
+++ types/srt-stream.d.ts
@@ -0,0 +1,53 @@
+
+import { Writable, Readable } from "stream";
+
+declare module "srt" {
+
+  interface SRTConnectionState {
+    readonly srt: SRT;
+    readonly socket: number;
+    readonly address: string;
+    readonly port: number;
+  }
+
+  interface SRTCallerState extends SRTConnectionState {
+    readonly fd: SRTFileDescriptor | null;
+    connect(callback: (state: SRTCallerState) => void);
+    close();
+  }
+
+  interface SRTListenerState extends SRTConnectionState {
+    listen(callback: (state: SRTListenerState) => void);
+  }
+
+  class SRTReadStream extends Readable implements SRTCallerState, SRTListenerState {
+
+    readonly srt: SRT;
+    readonly fd: SRTFileDescriptor | null;
+    readonly socket: number;
+    readonly address: string;
+    readonly port: number;
+    readonly readTimer: number | null;
+
+    constructor(address: string, port: number, opts?: unknown);
+
+    connect(callback: (state: SRTCallerState) => void);
+    close();
+
+    listen(callback: (state: SRTListenerState) => void);
+  }
+
+  class SRTWriteStream extends Writable implements SRTCallerState {
+
+    readonly srt: SRT;
+    readonly socket: number;
+    readonly address: string;
+    readonly port: number;
+    readonly fd: SRTFileDescriptor | null;
+
+    constructor(address: string, port: number, opts?: unknown);
+
+    connect(callback: (state: SRTCallerState) => void);
+    close();
+  }
+}
