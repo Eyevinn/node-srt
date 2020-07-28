27bf1be19dff3fb035c6ae6a6c732660247be703
Stephan Hesse <tchakabam@gmail.com>
2020-07-28 14:07:58 +0200

    index.d.s: add /* eslint-disable @typescript-eslint/triple-slash-reference */

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="./types/srt-api.d.ts" />
 /// <reference path="./types/srt-stream.d.ts" />
 /// <reference path="./types/srt-server.d.ts" />
