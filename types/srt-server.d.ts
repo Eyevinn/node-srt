b7f7247478f98983a18e584e5106a9d9f4d323ba
Stephan Hesse <stephan@emliri.com>
2020-10-19 23:43:18 +0200

    srt-server/connection typings

--- types/srt-server.d.ts
+++ types/srt-server.d.ts
@@ -1,22 +1,58 @@
 /// <reference types="node" />
 
-declare module "srt" {
+import {EventEmitter} from 'events';
+import { SRTResult, SRTSockOpt } from '../src/srt-api-enums';
+import { SRTSockOptValue } from './srt-api';
+import { AsyncSRT } from './srt-api-async';
 
-  import {EventEmitter} from 'events';
+export class AsyncReaderWriter {
+  constructor(asyncSrt: AsyncSRT, socketFd: number);
 
-  interface SRTServerBindOpts {
-    /**
-     * default: "0.0.0.0"
-     */
-    address?: string
+  writeChunks(buffer: Uint8Array | Buffer, mtuSize: number,
+    writesPerTick: number): Promise<void>;
 
-    port: number
-  }
-
-  type SRTServerEvent = "listening"; /*| "foobar" */
-
-  class SRTServer extends EventEmitter /*<SRTServerEvent>*/ {
-    listen(opts: SRTServerBindOpts): void
-  }
-
+  readChunks(minBytesRead: number,
+    readBufSize: number,
+    onRead: (buf: Uint8Array) => void,
+    onError: (readResult: (SRTResult.SRT_ERROR | null)) => void): Promise<Uint8Array[]>;
 }
+
+export class SRTConnection extends EventEmitter {
+  readonly fd: number;
+  readonly gotFirstData: boolean;
+
+  read(): Promise<Uint8Array | SRTResult.SRT_ERROR | null>;
+  write(chunk: Buffer | Uint8Array): Promise<SRTResult>;
+  close(): Promise<SRTResult | null>;
+
+  isClosed(): boolean;
+
+  onData(): void;
+
+  getReaderWriter(): AsyncReaderWriter;
+}
+
+export class SRTServer extends EventEmitter /*<SRTServerEvent>*/ {
+
+  static create(port: number, address?: string,
+    epollPeriodMs?: number): Promise<SRTServer>;
+
+  port: number;
+  address: string;
+  epollPeriodMs: number;
+  socket: number;
+  epid: number;
+
+  constructor(port: number, address?: string, epollPeriodMs?: number);
+
+  create(): Promise<SRTServer>;
+  open(): Promise<SRTServer>;
+  dispose(): Promise<SRTResult>;
+
+  setSocketFlags(opts: SRTSockOpt[], values: SRTSockOptValue[]): Promise<SRTResult[]>;
+
+  getConnectionByHandle(fd: number);
+  getAllConnections(): SRTConnection[];
+}
+
+
