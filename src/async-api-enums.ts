a5a26063b035aa9ed739653173456ba760aedc73
Stephan Hesse <stephan@emliri.com>
2020-10-19 23:43:05 +0200

    add srt-server and srt-connection (can manage multiple clients), - based on async-api - can be used with reader/writer (i.e the underlying modes)

---
+++ src/async-api-enums.ts
@@ -0,0 +1,13 @@
+export enum SRTServerEvent {
+  CREATED = "created",
+  CONNECTION = "connection",
+  DISCONNECTION = "disconnection",
+  DISPOSED = "disposed"
+}
+
+export enum SRTConnectionEvent {
+  DATA = "data",
+  CLOSING = "closing",
+  CLOSED = "closed"
+}
+
