22d5d61ea5d4e0ceaa049bef2cc38fee56a77aec
Stephan Hesse <stephan@emliri.com>
2020-10-19 23:37:47 +0200

    async-worker: enable using transferrable for zero-copy + allow better debugging (like in api/dispatcher side) + misc improvements on code quality

--- src/async-worker.js
+++ src/async-worker.js
@@ -1,41 +1,79 @@
 const {
-  Worker, isMainThread, parentPort, workerData
+  isMainThread, parentPort
 } = require('worker_threads');
 
+const debug = require('debug')('srt-async-worker');
+
 const { SRT } = require('../build/Release/node_srt.node');
+
+const { argsToString, traceCallToString, extractTransferListFromParams } = require('./async-helpers');
+
+const DEBUG = false;
+const DRY_RUN = false;
 
 if (isMainThread) {
   throw new Error("Worker module can not load on main thread");
 }
 
-(function run() {
-  const libSRT = new SRT();
+try {
+  run()
+} catch(err) {
+  console.error('AsyncSRT task-runner internal exception:', err);
+}
+
+function run() {
+
+  DEBUG && debug('AsyncSRT: Launching task-runner');
+
+  const srtNapiObjw = new SRT();
+
+  DEBUG && debug('AsyncSRT: SRT native object-wrap created');
+
+  parentPort.on('close', () => {
+    DEBUG && debug('AsyncSRT: Closing task-runner');
+  })
+
   parentPort.on('message', (data) => {
+
     if (!data.method) {
       throw new Error('Worker message needs `method` property');
     }
+
     /*
     if (!data.workId) {
       throw new Error('Worker message needs `workId` property');
     }
     */
-    let result = libSRT[data.method].apply(libSRT, data.args);
-    // TODO: see if we can do this using SharedArrayBuffer for example,
-    //       or just leveraging Transferable objects capabilities ... ?
-    // FIXME: Performance ... ?
-    if (result instanceof Buffer) {
-      const buf = Buffer.allocUnsafe(result.length);
-      result.copy(buf);
-      result = buf;
+
+    if (data.args.some((arg) => arg === undefined)) {
+      const err = new Error(
+        `Ignoring call: Can't have any arguments be undefined: ${argsToString(data.args)}`);
+      parentPort.postMessage(err);
+      return;
     }
+
+    DEBUG && debug('Received call:', traceCallToString(data.method, data.args));
+
+    let result = 0;
+    if (!DRY_RUN) {
+      try {
+        result = srtNapiObjw[data.method].apply(srtNapiObjw, data.args);
+      } catch(err) {
+        console.error(
+          `Exception thrown by native binding call "${traceCallToString(data.method, data.args)}":`,
+            err);
+        parentPort.postMessage({err, call: data});
+        return;
+      }
+    }
+
+    const transferList = extractTransferListFromParams([result]);
+
     parentPort.postMessage({
       // workId: data.workId,
       timestamp: data.timestamp,
       result
-    });
+    }, transferList);
+
   });
-})();
-
-
-
-
+}
